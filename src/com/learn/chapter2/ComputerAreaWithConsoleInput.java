41f7aa8e08f11f73c6ad78154c3f2b62edbe71e4
shnma <shnma@mail.nfsq.com.cn>
2019-08-31 17:37:38 +0800

    从控制台获取用户输入的半径计算圆的面积

--- src/com/learn/chapter2/ComputerAreaWithConsoleInput.java
+++ src/com/learn/chapter2/ComputerAreaWithConsoleInput.java
@@ -1,9 +1,27 @@
 package com.learn.chapter2;
 
+import java.util.Scanner;
+
 /**
- * @author : mashuainan
- * create at:  2019/8/31  下午5:14
- * @description: 从控制台获取用户输入的半径计算圆的面积
+ * description: 从控制台获取用户输入的半径计算圆的面积
+ * @author shnma
+ * @date 2019/8/31 下午5:15
  */
 public class ComputerAreaWithConsoleInput {
+
+	public static void main(String[] args){
+		// Create a Scanner object
+		Scanner scanner = new Scanner(System.in);
+
+		// Prompt the user to enter a radius
+		System.out.print("Enter a number for radius: ");
+		double radius = scanner.nextDouble();
+
+		// Computer area
+		double area = radius * radius * 3.1415926;
+
+		// Display result
+		System.out.println("The area for the circle of radius: "
+				+ radius + " is " + area);
+	}
 }
