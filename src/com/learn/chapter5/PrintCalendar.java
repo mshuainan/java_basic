3846b06e66aa24ed8c074fcce4dfd01acc67f1fd
shnma <shnma@mail.nfsq.com.cn>
2019-09-12 10:00:41 +0800

    打印日历

--- src/com/learn/chapter5/PrintCalendar.java
+++ src/com/learn/chapter5/PrintCalendar.java
@@ -32,7 +32,7 @@ public class PrintCalendar {
 	}
 
 	/**
-	 * description: A stub for printMonth may look like this
+	 * description: Print the calender for a month in a year
 	 * @param year
 	 * @param month
 	 * @return void
@@ -48,7 +48,7 @@ public class PrintCalendar {
 	}
 
 	/**
-	 * description: A stub for printMonthTitle may look like this
+	 * description: Print the month title , e.g.,May 1999
 	 * @param year
 	 * @param month
 	 * @return void
@@ -58,12 +58,11 @@ public class PrintCalendar {
 	public static void printMonthTitle(int year, int month){
 		System.out.println("		" + getMonthName(month) + " " + year);
 		System.out.println("----------------------------------");
-		System.out.println(" Sun  Mon  Tue  Wed  Thu  Fir  Sat");
-
+		System.out.println(" Sun Mon Tue Wed Thu Fir Sat");
 	}
 
 	/**
-	 * description: A stub for printMonthBody mqy look like this
+	 * description: Print month body
 	 * @param year
 	 * @param month
 	 * @return void
@@ -71,18 +70,35 @@ public class PrintCalendar {
 	 * @date 2019/9/11 下午7:19
 	 */
 	public static void printMonthBody(int year, int month){
+		// Get start day of the week for the first date in the month
+		int startDay = getStartDay(year,month);
 
+		// Get number of days in the month
+		int numberOfDaysInMonth = getNumberOfDaysInMonth(year,month);
+
+		// Pad space before the first day of the month
+		for (int i = 0; i < startDay; i++){
+			System.out.print("	");
+		}
+
+		for(int i = 1; i < numberOfDaysInMonth; i++){
+			System.out.printf("%4d", i);
+			if((i + startDay) % 7 == 0){
+				System.out.println();
+			}
+		}
+		System.out.println();
 	}
 
 	/**
-	 * description: A stub for getMonthName may look like this
+	 * description: Get the english name for the month
 	 * @param month
 	 * @return java.lang.String
 	 * @author shnma
 	 * @date 2019/9/11 下午7:21
 	 */
 	public static String getMonthName(int month){
-		String monthName = "";
+		String monthName;
 		switch (month){
 			case 1:
 				monthName = "January";
@@ -127,7 +143,7 @@ public class PrintCalendar {
 	}
 
 	/**
-	 * description: A stub for getStartDay may look like this
+	 * description: Get the start day of month/1/year
 	 * @param year
 	 * @param month
 	 * @return int
@@ -135,11 +151,17 @@ public class PrintCalendar {
 	 * @date 2019/9/11 下午7:23
 	 */
 	public static int getStartDay(int year, int month){
-		return 1;
+		final int START_DAY_FOR_JAN_1_1800 = 3;
+
+		// Grt total number of days from 1/1/1800 to month/1/year
+		int totalNumberOfDays = getTotalNumberOfDays(year,month);
+
+		// Return the start day for month/1/year
+		return (totalNumberOfDays + START_DAY_FOR_JAN_1_1800) % 7;
 	}
 
 	/**
-	 * description: A stub for getTotalNumberOfDays may look like this
+	 * description: Get the total number of days since January 1, 1800
 	 * @param year
 	 * @param month
 	 * @return int
@@ -147,29 +169,63 @@ public class PrintCalendar {
 	 * @date 2019/9/11 下午7:24
 	 */
 	public static int getTotalNumberOfDays(int year, int month){
-		return 10000;
+		int total = 0;
+
+		// Get the total days from 1800 to 1/1/year
+		for(int i = 1800; i < year; i++){
+			if(isLeapYear(i)){
+				total += 366;
+			} else {
+				total += 365;
+			}
+		}
+
+		// Add days from jan to the month prior to the calender month
+		for(int i = 0; i < month; i++){
+			total += getNumberOfDaysInMonth(year,i);
+		}
+
+		return total;
 	}
 
 	/**
-	 * description: A stub for getlNumberOfDaysInMonth may look like this
+	 * description: Get the number of day in a month
 	 * @param year
 	 * @param month
 	 * @return int
 	 * @author shnma
 	 * @date 2019/9/11 下午7:24
 	 */
-	public static int getlNumberOfDaysInMonth(int year, int month){
-		return 31;
+	public static int getNumberOfDaysInMonth(int year, int month){
+		if(month == 1 || month == 3 || month == 5 ||month == 7
+				|| month == 8 ||month == 10 || month == 12){
+			return 31;
+		}
+
+		if(month == 4 || month == 6 || month == 9 ||month == 11){
+			return 31;
+		}
+		if(month == 2){
+			return isLeapYear(year) ? 29 : 28;
+		}
+
+		// If month is incorrect
+		return 0;
 	}
 
 	/**
-	 * description:A stub for isLeapYear may look like this
+	 * description: Determine if it is a leap year
 	 * @param year
 	 * @return boolean
 	 * @author shnma
 	 * @date 2019/9/11 下午7:26
 	 */
 	public static boolean isLeapYear(int year){
-		return true;
+		if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+			return Boolean.TRUE;
+		} else {
+			return Boolean.FALSE;
+		}
 	}
+
 }
