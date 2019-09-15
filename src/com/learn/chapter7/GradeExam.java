88fe6f4ead95894c0ee87b86db1ef4c754f0936e
shnma <shnma@mail.nfsq.com.cn>
2019-09-15 09:19:33 +0800

    学生选择题正确率统计

--- src/com/learn/chapter7/GradeExam.java
+++ src/com/learn/chapter7/GradeExam.java
@@ -6,4 +6,39 @@ package com.learn.chapter7;
  * @description: 学生选择题正确率统计
  */
 public class GradeExam {
+	/**
+	 * description: Main method
+	 * @param args
+	 * @return void
+	 * @author shnma
+	 * @date 2019/9/14 下午11:22
+	 */
+	public static void main(String[] args) {
+		// Students answers to the questions
+		char[][] answers = {
+			{'A','B','A','C','C','D','E','E','A','D'},
+			{'A','B','A','C','C','D','E','E','A','D'},
+			{'A','B','A','C','C','D','E','E','A','D'},
+			{'A','B','A','C','C','D','E','E','A','D'},
+			{'A','B','A','C','C','D','E','E','A','D'},
+			{'A','B','A','C','C','D','E','E','A','D'},
+			{'A','B','A','C','C','D','G','E','A','D'},
+			{'A','B','A','C','C','D','E','E','A','D'}
+		};
+
+		// Key to the questions
+		char[] keys = {'A','B','C','C','C','C','E','E','A','D'};
+
+		// Grade all answers
+		for(int i = 0; i < answers.length; i++){
+			// Grade one student
+			int correctCount = 0;
+			for(int j = 0; j < answers[i].length; j++){
+				if(answers[i][j] == keys[j]){
+					correctCount++;
+				}
+			}
+			System.out.println("Student " + i + ",s correct count is " + correctCount);
+		}
+	}
 }
--- src/com/learn/chapter7/PassTwoDimensionalArray.java
+++ src/com/learn/chapter7/PassTwoDimensionalArray.java
@@ -1,9 +1,39 @@
 package com.learn.chapter7;
 
+import java.util.Scanner;
+
 /**
  * @author : mashuainan
  * create at:  2019/9/14  下午11:10
  * @description: 给方法传递二维数组
  */
 public class PassTwoDimensionalArray {
+
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner scanner = new Scanner(System.in);
+
+		//Enter array values
+		int[][] m = new int[3][4];
+		System.out.println("Enter " + m.length + " rows and " + m[0].length + "columns: ");
+		for(int i = 0; i < m.length; i++){
+			for(int j = 0; j < m[i].length; j++){
+				m[i][j] = scanner.nextInt();
+			}
+		}
+
+		// Display result
+		System.out.println("\nSum of all elements is " + sum(m));
+	}
+
+	public static int sum(int[][] m){
+		int total = 0;
+		for(int row = 0; row < m.length; row++){
+			for(int column = 0; column < m[row].length; column++){
+				total += m[row][column];
+			}
+		}
+
+		return total;
+	}
 }
