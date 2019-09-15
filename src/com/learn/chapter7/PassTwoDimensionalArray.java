0c5e484bc4f3d16d2fcdf5374e38f2c744d80821
shnma <shnma@mail.nfsq.com.cn>
2019-09-15 09:20:38 +0800

    给方法传递二维数组

--- src/com/learn/chapter7/PassTwoDimensionalArray.java
+++ src/com/learn/chapter7/PassTwoDimensionalArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * @author : mashuainan
  * create at:  2019/9/14  下午11:10
- * @description: 给方法传递二维数组
+ * description: 给方法传递二维数组
  */
 public class PassTwoDimensionalArray {
 
